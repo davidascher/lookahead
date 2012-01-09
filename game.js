b7b90551838b032153694cc1d60c8a0673229047
David Ascher <david.ascher@gmail.com>
2012-01-09 09:01:24 -0800

    updated version of hitbox addon

--- game.js
+++ game.js
@@ -25,12 +25,18 @@ var player2_walls = [
 
 /**@
 * #Collision
-* @category 2D
+* @category Collision
 * Components to display Crafty.polygon Array for debugging collision detection
+* * @example
+* ~~~
+* Crafty.e("2D,DOM,Player,Collision,WiredHitBox").collision(new Crafty.polygon([0,0],[0,300],[300,300],[300,0]))
+* ~~~
+* this will display a wired square over your original Canvas screen
 */
 Crafty.c("WiredHitBox", {
+
     init:function(){
-        
+
         if (Crafty.support.canvas){
             var c = document.getElementById('HitBox');
             if(!c){
@@ -45,7 +51,13 @@ Crafty.c("WiredHitBox", {
                 Crafty.stage.elem.appendChild(c);
             }
             var ctx = c.getContext('2d');
+            if(!this.map) this.collision();
+            var drawed = 0,total=Crafty("WiredHitBox").length;
             this.requires("Collision").bind("EnterFrame",function(){
+                if(drawed == total){
+                    ctx.clearRect(0,0,Crafty.viewport.width,Crafty.viewport.height);
+                    drawed = 0;
+                }
                 ctx.beginPath();
                 ctx.strokeStyle = 'white';
                 for(var p in this.map.points){
@@ -53,13 +65,21 @@ Crafty.c("WiredHitBox", {
                 }
                 ctx.closePath();
                 ctx.stroke();
+                drawed++;
+               
             });
         }
        
         return this;
     }
 });
-
+/*
+* @example
+* ~~~
+* Crafty.e("2D,DOM,Player,Collision,SolidHitBox").collision(new Crafty.polygon([0,0],[0,300],[300,300]))
+* ~~~
+* this will display a solid triangle over your original Canvas screen
+*/
 Crafty.c("SolidHitBox", {
     init:function(){
         if (Crafty.support.canvas){
@@ -76,13 +96,20 @@ Crafty.c("SolidHitBox", {
                 Crafty.stage.elem.appendChild(c);
             }
             var ctx = c.getContext('2d');
+            if(!this.map) this.collision();
+            var drawed = 0,total =Crafty("SolidHitBox").length;
             this.requires("Collision").bind("EnterFrame",function(){
+                  if(drawed == total){
+                    ctx.clearRect(0,0,Crafty.viewport.width,Crafty.viewport.height);
+                    drawed = 0;
+                }
                 ctx.beginPath();
                 for(var p in this.map.points){
                     ctx.lineTo(Crafty.viewport.x+this.map.points[p][0],Crafty.viewport.y+this.map.points[p][1]);
                 }
                 ctx.closePath();
                 ctx.fill();
+                drawed++;
             });
         }
         
@@ -467,6 +494,19 @@ window.onload = function () {
                 this.rotation += 45;
             })
         }
+        if (e.key == Crafty.keys.T) {
+            Crafty('ball').each(function() {
+                console.log(this.attr('wired'));
+                if (this.attr('wired')) {
+                    this.removeComponent('WiredHitBox');
+                    this.attr('wired', false);
+                } else {
+                    this.addComponent('WiredHitBox');
+                    this.attr('wired', true);
+                }
+                this.rotation += 45;
+            })
+        }
     })
     Crafty.bind("1or2?", function() {
         doMenu("(1) or (2) players?", {'1': '1moveorshoot?', '2': '2moveorshoot?'})
@@ -491,7 +531,7 @@ window.onload = function () {
         window.setTimeout(function() {
             gun.shooting = false;
         }, 1000);
-        ball = Crafty.e("2D, DOM, Persist, Collision, ball, WiredHitBox, Shooter, bounce")
+        ball = Crafty.e("2D, DOM, Persist, Collision, ball, Shooter, bounce")
             .attr({x: x, y: y, w: BALL_W, h: BALL_H})
             .origin("center")
             .collision()
